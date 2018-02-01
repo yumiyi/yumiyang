fcd13092026e057d6ac3eda3dcb3285ab72b56e4
yumiyang <yumiyang@qnap.com>
2018-02-01 23:18:14 +0800

    [Purpose] Optimize Code: Random select the members in the file.
    [Solution]
    
    1. Save text file with utf8.
    
    2. f = open('file;, 'r') replace with open file ('file', 'r', encoding = 'utf8') as f
    
    3. Install Pep8 pylint to fix coding style.

--- random/random.py
+++ random/random.py
@@ -2,8 +2,10 @@
 Purpose:Random choose the member in the file.
 '''
 import random
-member = open('C:\\Users\\ASUS\\Documents\\yumiyang\\random\\name.txt','r')
-file = member.read().split("\n")
-print (file)
-selectN = random.sample(file,10)
-print (selectN)
+
+with open("C:\\Users\\ASUS\\Documents\\yumiyang\\random\\name2.txt", 'r',
+          encoding='utf8') as f:
+    CONTENT = f.read().split("\n")
+print(CONTENT)
+SELECT = random.sample(CONTENT, 10)
+print(SELECT)
